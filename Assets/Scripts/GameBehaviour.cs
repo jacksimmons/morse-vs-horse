376f124f041472ce439bdff9c58f2bc3eba2be29
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2024-07-01 03:26:06 +0100

    First month

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -75,14 +75,12 @@ public class GameBehaviour : MonoBehaviour
         // Easy takes ~20s so spawn every 15s
         QueuePonySpawn(1, Difficulty.Easy);
         QueuePonySpawn(10, Difficulty.Easy);
-        //QueuePonySpawn(40, Difficulty.Easy);
-
-        //QueuePonySpawn(50, Difficulty.Hard);
-        //QueuePonySpawn(70, Difficulty.Hard);
-        //QueuePonySpawn(90, Difficulty.Hard);
+        QueuePonySpawn(20, Difficulty.Easy);
+        QueuePonySpawn(30, Difficulty.Easy);
+        QueuePonySpawn(40, Difficulty.Easy);
 
         // Activate victory check after all ponies have spawned
-        StartCoroutine(Wait.WaitThen(1, () => m_checkNoMessagesLeft = true));
+        StartCoroutine(Wait.WaitThen(40, () => m_checkNoMessagesLeft = true));
     }
 
 
